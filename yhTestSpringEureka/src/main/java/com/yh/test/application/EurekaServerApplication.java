81bb0921022fbfcd75415ecad7facdc16a500e46
k0yang <154332701@qq.com>
2017-03-08 11:23:20 +0800

    eureka 集群

--- yhTestSpringEureka/src/main/java/com/yh/test/application/EurekaServerApplication.java
+++ yhTestSpringEureka/src/main/java/com/yh/test/application/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableEurekaClient
 public class EurekaServerApplication {
 
     public static void main(String args[]){
